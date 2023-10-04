e3a22f503998441abb17915e467a53827e41f2dd
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2023-10-04 19:37:20 +0200

    practica 4 ejercicio 2

--- app/src/main/java/es/ua/eps/filmoteca/FilmListActivity.kt
+++ app/src/main/java/es/ua/eps/filmoteca/FilmListActivity.kt
@@ -3,11 +3,13 @@ package es.ua.eps.filmoteca
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.core.view.accessibility.AccessibilityViewCommand.SetTextArguments
 import es.ua.eps.filmoteca.databinding.ActivityFilmEditBinding
 import es.ua.eps.filmoteca.databinding.ActivityFilmListBinding
+
 
 class FilmListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,11 +19,18 @@ class FilmListActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val list = binding.list
+        val films = mutableListOf<Film>()
+        for (film: Film in FilmDataSource().films){
+            films.add(film)
+        }
 
-        val adapter = ArrayAdapter(
+        val adapter = FilmsAdapter(
             this,
-            android.R.layout.simple_list_item_1, FilmDataSource().films
+            R.layout.item_film, FilmDataSource().films
         )
+
+
+
         list.adapter = adapter
     }
 }
---
+++ app/src/main/java/es/ua/eps/filmoteca/FilmsAdapter.kt
@@ -0,0 +1,30 @@
+package es.ua.eps.filmoteca
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class FilmsAdapter(context: Context?, resource: Int,
+                   objects: List<Film>?
+                ) : ArrayAdapter<Film>(context!!, resource, objects!!){
+      override fun getView(position: Int, convertView: View?,
+                           parent: ViewGroup) : View {
+          var view : View = convertView?: LayoutInflater.from(this.context)
+                                .inflate(R.layout.item_film, parent, false)
+          val tvTitle = view.findViewById<TextView>(R.id.title) as TextView
+          val tvDirector = view.findViewById<TextView>(R.id.director) as TextView
+          val tvImage = view.findViewById<TextView>(R.id.itemImg) as ImageView
+
+          getItem(position)?.let{
+              tvTitle.text = it.title
+              tvDirector.text = it.director
+              tvImage.setImageResource(it.imagesResId)
+          }
+          return view
+      }
+}
