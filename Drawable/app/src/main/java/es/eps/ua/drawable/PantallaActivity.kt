28e573a3139d1ceef06a9ff9e141868738ee36af
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2023-10-29 10:48:06 +0100

    Set up for exercice 2

--- Drawable/app/src/main/java/es/eps/ua/drawable/PantallaActivity.kt
+++ Drawable/app/src/main/java/es/eps/ua/drawable/PantallaActivity.kt
@@ -1,11 +1,31 @@
 package es.eps.ua.drawable
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import es.eps.ua.drawable.databinding.ActivityMainBinding
+import es.eps.ua.drawable.databinding.ActivityPantallaBinding
 
 class PantallaActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding : ActivityPantallaBinding
+
+    private lateinit var buttonExc1 : Button
+    private lateinit var buttonExc2 : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pantalla)
+        viewBinding = ActivityPantallaBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        buttonExc1 = viewBinding.Exercice1
+        buttonExc2 = viewBinding.Exercice2
+
+        buttonExc1.setOnClickListener {
+            val intent = Intent(this@PantallaActivity, PantallaExercice1Activity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
